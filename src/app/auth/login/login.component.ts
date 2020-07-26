b645fde31f43c791593d3c708b116831e8996a9d
crimson666 <ankurbsws@gmail.com>
2020-07-27 02:28:22 +0530

    working on login function

---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { LoginRequestPayload } from './login-request.payload';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  
+  loginForm: FormGroup;
+  loginRequestPayload: LoginRequestPayload;
+
+  constructor( ) { 
+    this.loginRequestPayload = {
+      username: '',
+      password: ''
+    };
+  }
+
+  ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required)
+    });
+  }
+
+}
